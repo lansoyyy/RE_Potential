9fa2be5c0414ebd5cb41bc1181a1572522bce36b
Lans12345 <olanalans12345@gmail.com>
2024-12-13 09:46:40 +0800

    added ui

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:re_potential/tabs/about_us_tab.dart';
 import 'package:re_potential/utils/colors.dart';
 import 'package:re_potential/widgets/text_widget.dart';
 
@@ -264,89 +265,7 @@ class _HomeTabState extends State<HomeTab> {
           const SizedBox(
             height: 20,
           ),
-          TextWidget(
-            text: 'Contact Us',
-            fontSize: 48,
-            fontFamily: 'Bold',
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-
-          const SizedBox(height: 10),
-          // Contact Information
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // Address
-              ContactInfo(
-                icon: Icons.location_on,
-                title: "Address",
-                details: "123 Main Street\nCity, State, ZIP",
-              ),
-              // Email
-              ContactInfo(
-                icon: Icons.email,
-                title: "Email",
-                details: "contact@example.com",
-              ),
-              // Phone
-              ContactInfo(
-                icon: Icons.phone,
-                title: "Phone",
-                details: "+1 (555) 123-4567",
-              ),
-            ],
-          ),
-          const SizedBox(height: 40),
-          // Contact Form
-          SizedBox(
-            width: 550,
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Your Email Address',
-                hintStyle:
-                    const TextStyle(color: Colors.black45, fontFamily: 'Bold'),
-                filled: true,
-                fillColor: Colors.white.withOpacity(0.2),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: const BorderSide(
-                    color: Colors.black, // Border color
-                    width: 2.0, // Border width
-                  ),
-                ),
-                contentPadding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              ),
-              style: const TextStyle(color: Colors.black),
-            ),
-          ),
-          const SizedBox(height: 25),
-          ElevatedButton(
-            onPressed: () {
-              // Handle form submission
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: primary,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-            ),
-            child: const Text(
-              "Send Message",
-              style: TextStyle(
-                  fontSize: 16, fontFamily: 'Bold', color: Colors.white),
-            ),
-          ),
-          const SizedBox(height: 40),
-          // Footer Text
-          const Text(
-            "© 2024 Your Company. All rights reserved.",
-            style: TextStyle(
-                color: Colors.black26, fontSize: 14, fontFamily: 'Regular'),
-          ),
+          const AboutUsTab(),
           const SizedBox(
             height: 50,
           ),
--- lib/tabs/map_tab.dart
+++ lib/tabs/map_tab.dart
@@ -1,10 +1,129 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
-class MapTab extends StatelessWidget {
+class MapTab extends StatefulWidget {
   const MapTab({super.key});
 
   @override
+  State<MapTab> createState() => _MapTabState();
+}
+
+class _MapTabState extends State<MapTab> {
+  final searchController = TextEditingController();
+  String nameSearched = '';
+
+  String? selectedValue;
+
+  // List of items in the dropdown
+  final List<String> items = [
+    'Municipality 1',
+    'Municipality 2',
+    'Municipality 3',
+    'Municipality 4'
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Column();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 50,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 0.5,
+                      ),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: DropdownButton<String>(
+                      underline: const SizedBox(),
+                      value: selectedValue,
+                      hint: const Text('Select Municipality'),
+                      icon: const Icon(Icons.arrow_drop_down),
+                      items: items.map((String item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedValue = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 0.5,
+                      ),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextFormField(
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Bold',
+                          fontSize: 14),
+                      onChanged: (value) {
+                        setState(() {
+                          nameSearched = value;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                          ),
+                          hintText: 'Search',
+                          hintStyle:
+                              TextStyle(fontFamily: 'Regular', fontSize: 18),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.grey,
+                          )),
+                      controller: searchController,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 800,
+            child: FlutterMap(
+              options: const MapOptions(
+                initialCenter:
+                    LatLng(8.485383, 124.655940), // Center the map over London
+                initialZoom: 16,
+              ),
+              children: [
+                TileLayer(
+                  // Display map tiles from any source
+                  urlTemplate:
+                      'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+                  userAgentPackageName: 'com.example.app',
+                  // And many more recommended properties!
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
