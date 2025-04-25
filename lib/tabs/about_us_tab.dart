20adde13f0e38d1651e6241ae2fb2547d38f7826
Lans12345 <olanalans12345@gmail.com>
2025-04-25 08:16:24 +0800

    added about us func

--- lib/tabs/about_us_tab.dart
+++ lib/tabs/about_us_tab.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:re_potential/tabs/home_tab.dart';
 import 'package:re_potential/utils/colors.dart';
 import 'package:re_potential/widgets/text_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class AboutUsTab extends StatelessWidget {
+class AboutUsTab extends StatefulWidget {
   const AboutUsTab({super.key});
 
+  @override
+  State<AboutUsTab> createState() => _AboutUsTabState();
+}
+
+class _AboutUsTabState extends State<AboutUsTab> {
+  final name = TextEditingController();
+  final msg = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -28,19 +36,20 @@ class AboutUsTab extends StatelessWidget {
             ContactInfo(
               icon: Icons.location_on,
               title: "Address",
-              details: "123 Main Street\nCity, State, ZIP",
+              details:
+                  "Claro M. Recto Avenue, Lapasan, 9000 Cagayan de Oro City, Philippines",
             ),
             // Email
             ContactInfo(
               icon: Icons.email,
               title: "Email",
-              details: "contact@example.com",
+              details: "ferdieannvaldehuesa@gmail.com",
             ),
             // Phone
             ContactInfo(
               icon: Icons.phone,
               title: "Phone",
-              details: "+1 (555) 123-4567",
+              details: "09563473134",
             ),
           ],
         ),
@@ -49,6 +58,7 @@ class AboutUsTab extends StatelessWidget {
         SizedBox(
           width: 550,
           child: TextField(
+            controller: name,
             decoration: InputDecoration(
               hintText: 'Your Email Address',
               hintStyle:
@@ -68,10 +78,46 @@ class AboutUsTab extends StatelessWidget {
             style: const TextStyle(color: Colors.black),
           ),
         ),
+        const SizedBox(height: 10),
+        // Contact Form
+        SizedBox(
+          width: 550,
+          child: TextField(
+            controller: msg,
+            maxLines: 5,
+            decoration: InputDecoration(
+              hintText: 'Message here',
+              hintStyle:
+                  const TextStyle(color: Colors.black45, fontFamily: 'Bold'),
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.2),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: const BorderSide(
+                  color: Colors.black, // Border color
+                  width: 2.0, // Border width
+                ),
+              ),
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            ),
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
         const SizedBox(height: 25),
         ElevatedButton(
-          onPressed: () {
-            // Handle form submission
+          onPressed: () async {
+            final Uri emailLaunchUri = Uri(
+              scheme: 'mailto',
+              path: 'ferdieannvaldehuesa@gmail.com',
+              queryParameters: {'subject': 'RE Potential', 'body': msg.text},
+            );
+
+            if (await canLaunchUrl(emailLaunchUri)) {
+              await launchUrl(emailLaunchUri);
+            } else {
+              print('Could not launch email client');
+            }
           },
           style: ElevatedButton.styleFrom(
             backgroundColor: primary,
